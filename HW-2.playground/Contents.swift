de524ac05555c7f9790a6b2fe849257c73c19373
michail <mihail.chertov@gmail.com>
2023-10-12 15:25:46 +0300

    add formatter + year

--- HW-2.playground/Contents.swift
+++ HW-2.playground/Contents.swift
@@ -2,27 +2,30 @@ import Foundation
 
 //1. Написать функцию, которая на вход принимает число: сумма, которую пользователь хочет положить на вклад, следующий аргумент это годовой процент, третий аргумент это срок Функция возвращает сколько денег получит пользователь по итогу.
 
-func howMuchmoney(money: Float, percent: Float) -> Float {
-    var myIncome: Float = money / 100 * percent
-    print("ваш доход с суммы:"); printMoney(money: money)
-    print("под процент: \(percent)%")
-    print("составит:"); printMoney(money: myIncome)
-    myIncome += money
-    print("Итоговая сумма:")
-    printMoney(money: myIncome)
-    return myIncome
-}
-func printMoney(money: Float) {
+func printMoneyFormatter(money: Float) -> String {
     let numberFormatter: NumberFormatter = NumberFormatter()
     numberFormatter.groupingSeparator = " "
     numberFormatter.groupingSize = 3
     numberFormatter.usesGroupingSeparator = true
     numberFormatter.decimalSeparator = "."
     numberFormatter.numberStyle = NumberFormatter.Style.decimal
-    guard let stringFromNumber = numberFormatter.string(from: money as NSNumber) as String? else { return }
+    guard let stringFromNumber = numberFormatter.string(from: money as NSNumber) as String? else { return "&"}
     print(stringFromNumber)
+    return stringFromNumber
 }
-howMuchmoney(money: 1300000, percent: 9.7)
+
+func percentSumMoreYear(money: Float, percent: Float, year: UInt) -> Float {
+    var percentFLoat: Float = percent / 100
+    var myIncome: Float = money * pow((1 + percentFLoat), Float(year))
+    let moneyPrint = printMoneyFormatter(money: money)
+    let myIncomePrint = printMoneyFormatter(money: myIncome)
+    print("ваш доход с суммы: \(moneyPrint)")
+    print("составит: \(myIncomePrint)")
+    return myIncome
+}
+
+percentSumMoreYear(money: 1000, percent: 10, year: 3)
+
 
 //2. Создать перечисление, которое содержит 3 вида пиццы и создать переменные с каждым видом пиццы.
 enum PizzaView: String {
