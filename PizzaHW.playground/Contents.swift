a30152262fae8be9f72a5d2854442a55189c4deb
michail <mihail.chertov@gmail.com>
2023-10-22 22:02:43 +0300

    hw-5

--- PizzaHW.playground/Contents.swift
+++ PizzaHW.playground/Contents.swift
@@ -1,20 +1,38 @@
+extension Array {
+    var toPrint: String  {
+        var str = ""
+        for element in self {
+            str += "\(element) "
+        }
+        return str
+    }
+}
 
-/*
-1. Создать перечисление с видами пиццы (хотя бы 4 - 5 кейсов)
-2. Создать структуру пиццы, она должна содержать:
- - стоимость,
- - вид пиццы,
- - толстое или тонкое тесто
- - и добавки (например, дополнительно добавить пепперони, помидоры, сыр).
- Вид пиццы должен быть вложенным типом для структуры пиццы. Подсказка: добавки лучше также сделать перечислением.
-3. Создать класс пиццерии, добавить массив с возможными пиццами. Переменная с массивом должна быть приватной. Массив задается в инициализаторе.
-4. Написать в классе пиццерии функции для добавления новой пиццы и для получения всех доступных пицц.
-5. Создать экземпляр класса пиццерии и добавить в него несколько пицц.
-*/
+protocol MenuProtocol {
+    var cost: Double {get}
+    
+    func descriptionTitle() -> String
+}
 
 
 
-struct ModelPizza {
+struct ModelPotatoFree: MenuProtocol {
+    
+    enum SizeBoxPotatoFree: String {
+        case small = "маленькая"
+        case midle = "средняя"
+        case big = "большая"
+    }
+    
+    var cost: Double
+    var size: SizeBoxPotatoFree
+    
+    func descriptionTitle() -> String {
+        "\(size.rawValue) картошка, цена \(cost)"
+    }
+}
+
+struct ModelPizza: MenuProtocol {
     
     enum PizzaView: String {
         case pepperoni = "'пепперони'"
@@ -41,98 +59,66 @@ struct ModelPizza {
     let dough: Dough
     var supplementation: [Supplementation]? = nil
     
+    func descriptionTitle() -> String {
+        if supplementation != nil {
+            "\(viewPizza.rawValue) на \(dough.rawValue) тесте, c добавками \(supplementation?.map({ $0.rawValue }).toPrint ?? " "), цена: \(cost) ₽"
+        } else {
+            "\(viewPizza.rawValue) на \(dough.rawValue) тесте, цена: \(cost) ₽"
+        }
+    }
 }
-/*
-3. - Создать класс пиццерии, добавить массив с возможными пиццами.
-   - Переменная с массивом должна быть приватной.
-   - Массив задается в инициализаторе.
-4. Написать в классе пиццерии функции 
-   - для добавления новой пиццы
-   - и для получения всех доступных пицц.
-5. Создать экземпляр класса пиццерии и добавить в него несколько пицц.
-*/
+
 
 class Pizzeria {
     
-    private var pizzas: [ModelPizza]
+    private var pizzaria: [MenuProtocol]
     
-    init(pizzas: [ModelPizza]) {
-        self.pizzas = pizzas
+    init(pizzas: [MenuProtocol]) {
+        self.pizzaria = pizzas
     }
     
-    func addPizza(add: ModelPizza) {
-        pizzas.append(add)
+    func addPosition(add: MenuProtocol) {
+        pizzaria.append(add)
     }
-    
-    //Получаем заказ
-    func takePizza(pizza: ModelPizza) {
-        if pizza.supplementation != nil {
-            print("Ваша пицца \(pizza.viewPizza.rawValue) на \(pizza.dough.rawValue) тесте, c добавками \(pizza.supplementation?.map({ $0.rawValue }).toPrint ?? " ") готова, цена: \(pizza.cost) ₽")
-        } else {
-            print("Ваша пицца \(pizza.viewPizza.rawValue) на \(pizza.dough.rawValue) тесте, готова, цена: \(pizza.cost) ₽")
+ 
+    func returnAllTitle() -> [MenuProtocol] {
+        for (index, title) in pizzaria.enumerated() {
+            print("\(index + 1) - \(title.descriptionTitle())")
         }
+        return pizzaria
     }
     
-    //Делаем заказ
-    func makeAnOrder(pizza: Int, add: [ModelPizza.Supplementation]?) -> ModelPizza {
-        var result = pizzas[pizza - 1]
-        print(result)
-        if  let supplement = add {
-            result.cost += 100
-            result.supplementation = add
-            print("Ваша пицца \(result.viewPizza.rawValue) на \(result.dough.rawValue) тесте, c добавками \(result.supplementation?.map({ $0.rawValue }).toPrint ?? " ") готова, цена: \(result.cost) ₽")
-            return result
+    func returnTitle(number: Int) -> MenuProtocol {
+        for (index, _) in pizzaria.enumerated() {
+            if number == index + 1 {
+                return pizzaria[number]
+            } else {
+                break
+            }
         }
-        print("Ваша пицца \(result.viewPizza.rawValue) на \(result.dough.rawValue) тесте, готова, цена: \(result.cost) ₽")
-        return result
-    }
-    
-    
-    
-    //Выводит все доступные пиццы
-    func printPizza() {
-        for (index, pizza) in pizzas.enumerated() {
-            print("Номер пиццы - \(index + 1):\n \(pizza.viewPizza.rawValue) на \(pizza.dough.rawValue) тесте, цена: \(pizza.cost) ₽")
-        }
-    }
-    
-    func returnPizzas() -> [ModelPizza] {
-        pizzas
+        return pizzaria[0]
     }
 }
 
 var pizzeria = Pizzeria(pizzas: [
-    ModelPizza(cost: 800.0, viewPizza: .cheese, dough: .thick),
-    ModelPizza(cost: 850, viewPizza: .cheese, dough: .thin),
-    ModelPizza(cost: 650, viewPizza: .pepperoni, dough: .thin),
-    ModelPizza(cost: 670, viewPizza: .pepperoni, dough: .thick),
-    ModelPizza(cost: 1200, viewPizza: .seaFood, dough: .thick),
-    ModelPizza(cost: 1350, viewPizza: .seaFood, dough: .thin),
-    ModelPizza(cost: 900, viewPizza: .pineapple, dough: .thin),
-    ModelPizza(cost: 950, viewPizza: .pineapple, dough: .thick),
+    ModelPizza(cost: 1200, viewPizza: .cheese, dough: .thin),
+    ModelPizza(cost: 1300, viewPizza: .cheese, dough: .thick, supplementation: [.cheese]),
+    ModelPotatoFree(cost: 70, size: .small)
 ])
-pizzeria.printPizza()
-pizzeria.addPizza(add: ModelPizza(cost: 490, viewPizza: .margarita, dough: .thin))
+
+pizzeria.addPosition(add: ModelPizza(cost: 490, viewPizza: .margarita, dough: .thin))
+pizzeria.addPosition(add: ModelPotatoFree(cost: 120, size: .big))
+pizzeria.returnAllTitle()
+
+let myOrder = pizzeria.returnTitle(number: 1)
 print(" ")
-pizzeria.printPizza()
-
-var myPizza = pizzeria.makeAnOrder(pizza: 9, add: [.cheese, .tomato])
-var anotherPizza = pizzeria.makeAnOrder(pizza: 3, add: nil)
-print(" ")
-
-pizzeria.takePizza(pizza: myPizza)
-pizzeria.takePizza(pizza: anotherPizza)
+print(myOrder.descriptionTitle())
 
 
 
-
-extension Array {
-    var toPrint: String  {
-        var str = ""
-        for element in self {
-            str += "\(element) "
-        }
-        return str
-    }
+func subtractionExample<T: Numeric>(a: T , b : T) -> T {
+   a - b
 }
+
+subtractionExample(a: 31, b: 20.5)
 
